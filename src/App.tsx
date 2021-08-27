5708df4ace77c8867a7e03a027a980ca93e39dd1
spleekz <Shutnik-12K@yandex,ru>
2021-08-27 18:02:25 +0300

    create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin:0;
+    padding:0
+  }
+`
 const AppContainer = styled.div``
 
 export const App: FC = (): JSX.Element => {
