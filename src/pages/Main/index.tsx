c583c2a48a7f4bf7fc70099ede5b19144f7704d0
spleekz <Shutnik-12K@yandex,ru>
2021-08-28 04:34:34 +0300

    add pushing to maze page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import { InputsSection } from './InputsSection'
 
@@ -7,10 +8,16 @@ const MainContainer = styled.div``
 const CreateMazesButton = styled.button``
 
 export const Main: FC = (): JSX.Element => {
+  const history = useHistory()
+
+  const goToMazePage = () => {
+    history.push('/maze')
+  }
+
   return (
     <MainContainer>
       <InputsSection />
-      <CreateMazesButton>Create Mazes</CreateMazesButton>
+      <CreateMazesButton onClick={goToMazePage}>Create Mazes</CreateMazesButton>
     </MainContainer>
   )
 }
