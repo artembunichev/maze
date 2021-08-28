f4684b25b7105447d887c39d1b8f339b5cf5d728
spleekz <Shutnik-12K@yandex,ru>
2021-08-28 22:51:09 +0300

    add rendering cells with border

--- src/pages/Maze/Maze.tsx
+++ src/pages/Maze/Maze.tsx
@@ -1,15 +1,28 @@
 import React, { FC, useState } from 'react'
 import styled from 'styled-components'
 import { useStore } from '../../stores/RootStore/RootStoreContext'
+import { IBorder } from '../../stores/MazeStore'
 
 interface CellContainerProps {
   cellSize: number
+  border: IBorder
+}
+interface MazeContainerProps {
+  mazeWidth: number
 }
 
-const MazeContainer = styled.div``
+const MazeContainer = styled.div<MazeContainerProps>`
+  display: flex;
+  flex-wrap: wrap;
+  width: ${(props) => props.mazeWidth}px;
+`
 const CellContainer = styled.div<CellContainerProps>`
   width: ${(props) => props.cellSize}px;
   height: ${(props) => props.cellSize}px;
+  border-left: ${(props) => `1px solid ${props.border.left ? `#000000` : `#c5ecf1`}`};
+  border-top: ${(props) => `1px solid ${props.border.top ? `#000000` : `#c5ecf1`}`};
+  border-right: ${(props) => `1px solid ${props.border.right ? `#000000` : `#c5ecf1`}`};
+  border-bottom: ${(props) => `1px solid ${props.border.bottom ? `#000000` : `#c5ecf1`}`};
 `
 
 export const Maze: FC = (): JSX.Element => {
@@ -18,12 +31,8 @@ export const Maze: FC = (): JSX.Element => {
   const [mazeStore] = useState(bindCreateMazeStore)
 
   const cells = mazeStore.cellsArray.map((c) => {
-    return (
-      <CellContainer cellSize={mazeStore.cellSize} key={c.id}>
-        {c.id[c.id.length - 1]}
-      </CellContainer>
-    )
+    return <CellContainer cellSize={mazeStore.cellSize} border={c.border} key={c.id}></CellContainer>
   })
 
-  return <MazeContainer>{cells}</MazeContainer>
+  return <MazeContainer mazeWidth={(mazeStore.cellSize + 2) * mazeStore.width}>{cells}</MazeContainer>
 }
