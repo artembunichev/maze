bd7bf6b8e3a5cb7d17b04cbe818205b1b1cda475
spleekz <Shutnik-12K@yandex,ru>
2021-08-28 22:15:55 +0300

    add maze store

---
+++ src/stores/MazeStore.ts
@@ -0,0 +1,57 @@
+import { makeAutoObservable } from 'mobx'
+import { IAppStore } from './AppStore'
+import uniqid from 'uniqid'
+
+export interface IBorder {
+  left: boolean
+  top: boolean
+  right: boolean
+  bottom: boolean
+}
+export interface ICell {
+  border: IBorder
+  id: string
+}
+export interface IMazeStore {
+  AppStore: IAppStore
+  widthOfCell: number
+  numberOfCells: number
+  width: number
+  height: number
+  cellsArray: Array<ICell>
+}
+
+export class MazeStore implements IMazeStore {
+  constructor(AppStore: IAppStore) {
+    makeAutoObservable(this)
+    this.AppStore = AppStore
+  }
+  AppStore: IAppStore
+
+  widthOfCell = 50
+
+  get width(): number {
+    return this.AppStore.mazeWidth
+  }
+  get height(): number {
+    return this.AppStore.mazeHeight
+  }
+  get numberOfCells(): number {
+    return this.width * this.height
+  }
+  get cellsArray(): Array<ICell> {
+    const arr: Array<ICell> = []
+    for (let i = 0; i <= this.numberOfCells; i++) {
+      arr.push({
+        border: {
+          left: true,
+          top: true,
+          right: true,
+          bottom: true,
+        },
+        id: uniqid(),
+      })
+    }
+    return arr
+  }
+}
--- src/stores/RootStore/RootStore.ts
+++ src/stores/RootStore/RootStore.ts
@@ -1,9 +1,14 @@
-import { IAppStore, AppStore } from '../AppStore';
+import { IAppStore, AppStore } from '../AppStore'
+import { IMazeStore, MazeStore } from '../MazeStore'
 
 export interface IRootStore {
   AppStore: IAppStore
+  createMazeStore(): IMazeStore
 }
 
 export class RootStore implements IRootStore {
   AppStore = new AppStore()
+  createMazeStore(): IMazeStore {
+    return new MazeStore(this.AppStore)
+  }
 }
