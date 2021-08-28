4b3e3a6e96e2a8953dc91f1a6784bfd48455f42b
spleekz <Shutnik-12K@yandex,ru>
2021-08-28 04:34:23 +0300

    add maze page component

---
+++ src/pages/Maze/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+const MazePageContainer = styled.div``
+
+export const MazePage: FC = (): JSX.Element => {
+  return <MazePageContainer>MAZE</MazePageContainer>
+}
