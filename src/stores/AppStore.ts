2776ad762e80c680b77bdea055f087ea84cf42e9
spleekz <Shutnik-12K@yandex,ru>
2021-08-28 04:13:00 +0300

    add app store

---
+++ src/stores/AppStore.ts
@@ -0,0 +1,31 @@
+import { makeAutoObservable } from 'mobx'
+
+export interface IAppStore {
+  numberOfMazes: number
+  mazeWidth: number
+  mazeHeight: number
+
+  setNumberOfMazes: (number: number) => void
+  setMazeWidth: (width: number) => void
+  setMazeHeight: (height: number) => void
+}
+
+export class AppStore implements IAppStore {
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  numberOfMazes = 3
+  mazeWidth = 25
+  mazeHeight = 25
+
+  setNumberOfMazes(number: number): void {
+    this.numberOfMazes = number
+  }
+  setMazeWidth(width: number): void {
+    this.mazeWidth = width
+  }
+  setMazeHeight(height: number): void {
+    this.mazeHeight = height
+  }
+}
