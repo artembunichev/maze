199eb6f74c27642328ee7c26429df60fb8851d28
spleekz <Shutnik-12K@yandex,ru>
2021-08-29 21:21:36 +0300

    upgrade keyboard hook

--- src/hooks/useKeyboard.tsx
+++ src/hooks/useKeyboard.tsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 
-export const useKeyboard = (): string | null => {
+export const useKeyboard = (): [string | null, boolean] => {
   const [key, setKey] = useState<string | null>(null)
+  const [isKeyPressed, setIsKeyPressed] = useState<boolean>(false)
 
   useEffect(() => {
-    const setPressedKey = (ev: KeyboardEvent): void => {
+    const onKeyDown = (ev: KeyboardEvent): void => {
+      setIsKeyPressed(false)
       setKey(ev.key)
     }
-    window.addEventListener('keydown', setPressedKey)
+    const onKeyUp = () => {
+      setIsKeyPressed(true)
+    }
+    window.addEventListener('keydown', onKeyDown)
+    window.addEventListener('keyup', onKeyUp)
 
-    return () => window.removeEventListener('keydown', setPressedKey)
+    return () => {
+      window.removeEventListener('keydown', onKeyDown)
+      window.removeEventListener('keyup', onKeyUp)
+    }
   }, [])
 
-  return key
+  return [key, isKeyPressed]
 }
