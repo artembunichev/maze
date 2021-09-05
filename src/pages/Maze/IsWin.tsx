7c0949cba9d393bfcedc63b4fde0a91364e2fe7a
spleekz <Shutnik-12K@yandex,ru>
2021-09-05 22:47:53 +0300

    add choosing size for new maze

--- src/pages/Maze/IsWin.tsx
+++ src/pages/Maze/IsWin.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import styled from 'styled-components'
 import { IMazeStore } from '../../stores/MazeStore'
 import { useStore } from '../../stores/RootStore/RootStoreContext'
@@ -11,18 +11,28 @@ interface IsWinProps {
 const IsWinContainer = styled.div``
 const IsWinTitle = styled.div``
 const RestartGameButton = styled.button``
+const RestartDescription = styled.div``
+const NewMazeSizeInput = styled.input``
 
 export const IsWin: FC<IsWinProps> = ({ store, updateStore }): JSX.Element => {
   const rootStore = useStore()
+  const { AppStore } = rootStore
+  const [newMazeSize, setNewMazeSize] = useState<number>(AppStore.mazeSize)
 
   const restartGame = () => {
+    AppStore.setMazeSize(newMazeSize)
     updateStore(() => rootStore.createMazeStore.bind(rootStore)())
     store.setIsWin(false)
   }
-  
+
   return (
     <IsWinContainer>
       <IsWinTitle>Win!</IsWinTitle>
+      <RestartDescription>
+        Generate new maze width
+        <NewMazeSizeInput value={newMazeSize} onChange={(e) => setNewMazeSize(Number(e.target.value))} />
+        cells
+      </RestartDescription>
       <RestartGameButton onClick={restartGame}>Generate new Maze</RestartGameButton>
     </IsWinContainer>
   )
