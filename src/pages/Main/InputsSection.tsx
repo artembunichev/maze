3e7e9c4208e62e310896a98366780d8e33342ffa
spleekz <Shutnik-12K@yandex,ru>
2021-09-05 22:47:01 +0300

    set maze size only by click

--- src/pages/Main/InputsSection.tsx
+++ src/pages/Main/InputsSection.tsx
@@ -1,23 +1,21 @@
 import { observer } from 'mobx-react-lite'
 import React, { FC } from 'react'
 import styled from 'styled-components'
-import { useStore } from '../../stores/RootStore/RootStoreContext'
-import { setValueFromInput } from '../../utils/setValueFromInput'
+
+interface InputSectionProps {
+  mazeSize: number
+  setMazeSize: React.Dispatch<React.SetStateAction<number>>
+}
 
 const InputSectionContainer = styled.div``
 const MazeSizeInputsContainer = styled.div``
 const MazeSizeInput = styled.input``
 
-export const InputsSection: FC = observer((): JSX.Element => {
-  const { AppStore } = useStore()
-
+export const InputsSection: FC<InputSectionProps> = observer(({ mazeSize, setMazeSize }): JSX.Element => {
   return (
     <InputSectionContainer>
       <MazeSizeInputsContainer>
-        <MazeSizeInput
-          value={AppStore.mazeSize}
-          onChange={(e) => setValueFromInput(e, AppStore.setMazeSize, AppStore)}
-        />
+        <MazeSizeInput value={mazeSize} onChange={(e) => setMazeSize(Number(e.target.value))} />
       </MazeSizeInputsContainer>
     </InputSectionContainer>
   )
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import styled from 'styled-components'
 import { InputsSection } from './InputsSection'
 import { useStore } from '../../stores/RootStore/RootStoreContext'
@@ -9,14 +9,15 @@ const CreateMazesButton = styled.button``
 
 export const Main: FC = (): JSX.Element => {
   const { AppStore } = useStore()
-
+  const [mazeSize, setMazeSize] = useState<number>(AppStore.mazeSize)
   const goToMazePage = () => {
+    AppStore.setMazeSize(mazeSize)
     AppStore.setIsGame(true)
   }
 
   return (
     <MainContainer>
-      <InputsSection />
+      <InputsSection mazeSize={mazeSize} setMazeSize={setMazeSize} />
       <CreateMazesButton onClick={goToMazePage}>Create Mazes</CreateMazesButton>
     </MainContainer>
   )
